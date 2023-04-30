43f3b87107a1f700ad5d224fde8e444a2c739d96
khy99 <khy991223@gmail.com>
2023-04-30 23:41:33 +0900

    2023.04.30. 1. 외부 갤러리 어플리케이션 접근 시 접근 권한 동의 임시 삭제
    2. GalleryActivity.java 대거 수정
     2.1 textView를 분석 결과와 처치법의 두 부분으로 이분화
     2.2 onActivityResult 삭제 및 processSelectedImage 추가
     2.3 ActivityResultLauncher를 사용하여 이미지 처리를 완전히 처리함.
     2.4 getTreatmentForAcne 추가
    
    3. 메인 메뉴 배치 변경

--- app/src/main/java/com/example/acneapplication/GalleryActivity.java
+++ app/src/main/java/com/example/acneapplication/GalleryActivity.java
@@ -18,11 +18,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.IOException;
@@ -42,7 +45,10 @@ public class GalleryActivity extends AppCompatActivity {
     public static final int PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 2;
     private ClassifierWithModel cls;
     private ImageView imageView;
-    private TextView textView;
+    private TextView resultTextView;
+
+    private TextView treatmentTextView;
+
 
     private Runnable onPermissionGranted;
 
@@ -59,6 +65,9 @@ public class GalleryActivity extends AppCompatActivity {
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
                         PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
             }
+        } else {
+            // 권한이 이미 허용되었을 경우, 콜백을 실행합니다.
+            onPermissionGranted.run();
         }
 
         // 콜백을 저장합니다.
@@ -71,20 +80,22 @@ public class GalleryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallary);
 
+        // 로그 추가
+        Log.d(TAG, "onCreate()");
+
         firestore = FirebaseFirestore.getInstance();
 
         Button selectBtn = findViewById(R.id.selectBtn);
         selectBtn.setOnClickListener(v -> {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                requestReadExternalStoragePermission(this::getImageFromGallery);
-            } else {
-                getImageFromGallery();
-            }
+            Log.d(TAG, "selectBtn clicked"); // 버튼 클릭 로그 추가
+            getImageFromGallery();
         });
 
 
+
         imageView = findViewById(R.id.imageView);
-        textView = findViewById(R.id.textView);
+        resultTextView = findViewById(R.id.resultTextView);
+        treatmentTextView = findViewById(R.id.treatmentTextView);
 
         cls = new ClassifierWithModel(this);
         try {
@@ -114,71 +125,67 @@ public class GalleryActivity extends AppCompatActivity {
         }
     }
 
+    private final ActivityResultLauncher<Intent> galleryActivityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                            Uri selectedImageUri = result.getData().getData();
+
+                            // 이미지 처리 로직을 여기에 추가하세요.
+                            processSelectedImage(selectedImageUri);
+                        }
+                    });
 
 
-    private void getImageFromGallery(){
+    private void getImageFromGallery() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT).setType("image/*");
-//        Intent intent = new Intent(Intent.ACTION_PICK,
-//                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(intent, GALLERY_IMAGE_REQUEST_CODE);
+        galleryActivityResultLauncher.launch(intent);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    private void processSelectedImage(Uri selectedImage) {
+        Bitmap bitmap = null;
 
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK &&
-                requestCode == GALLERY_IMAGE_REQUEST_CODE) {
-            if(data == null) {
-                return;
+        try {
+            if (Build.VERSION.SDK_INT >= 29) {
+                ImageDecoder.Source src
+                        = ImageDecoder.createSource(getContentResolver(), selectedImage);
+                bitmap = ImageDecoder.decodeBitmap(src);
+            } else {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
             }
-
-            Uri selectedImage = data.getData();
-            Bitmap bitmap = null;
-
-            try {
-                if(Build.VERSION.SDK_INT >= 29) {
-                    ImageDecoder.Source src
-                            = ImageDecoder.createSource(getContentResolver(), selectedImage);
-                    bitmap = ImageDecoder.decodeBitmap(src);
-                } else {
-                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
-                }
-            } catch (IOException ioe) {
-                Log.e(TAG, "Falied to read Image", ioe);
-            }
-
-            if(bitmap != null) {
-                Pair<String, Float> output = cls.classify(bitmap);
-                String resultStr = String.format(Locale.ENGLISH,
-                        "여드름 종류 : %s, 확률 : %.2f%%",
-                        output.first, output.second * 100);
-
-                textView.setText(resultStr);
-                imageView.setImageBitmap(bitmap);
-
-
-                // 이미지 파일 이름 추출
-                String imageName = getImageNameFromUri(selectedImage);
-
-                // 현재 시간을 문자열로 변환
-                String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
-
-
-                // 결과를 Firestore에 저장
-                Map<String, Object> classificationData = new HashMap<>();
-                classificationData.put("imageName", imageName);
-                classificationData.put("result", resultStr);
-                classificationData.put("timestamp", timeStamp);
-
-                firestore.collection("classifications")
-                        .add(classificationData)
-                        .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
-                        .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
-            }
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to read Image", ioe);
         }
 
+        if (bitmap != null) {
+            Pair<String, Float> output = cls.classify(bitmap);
+            String resultStr = String.format(Locale.ENGLISH,
+                    "여드름 종류 : %s, 확률 : %.2f%%",
+                    output.first, output.second * 100);
 
+            resultTextView.setText(resultStr);
+            imageView.setImageBitmap(bitmap);
+
+            // 분류명에 따른 처치법을 firestore에서 가져옴
+            getTreatmentForAcne(output.first);
+
+            // 이미지 파일 이름 추출
+            String imageName = getImageNameFromUri(selectedImage);
+
+            // 현재 시간을 문자열로 변환
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+
+            // 결과를 Firestore에 저장
+            Map<String, Object> classificationData = new HashMap<>();
+            classificationData.put("imageName", imageName);
+            classificationData.put("result", resultStr);
+            classificationData.put("timestamp", timeStamp);
+
+            firestore.collection("classifications")
+                    .add(classificationData)
+                    .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                    .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
+        }
     }
 
     private String getImageNameFromUri(Uri uri) {
@@ -203,6 +210,27 @@ public class GalleryActivity extends AppCompatActivity {
         }
         return imageName;
     }
+
+    private void getTreatmentForAcne(String acneType) {
+        firestore.collection("acne_treatments")
+                .document(acneType)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            String treatment = document.getString("treatment");
+                            treatmentTextView.append("\n\n처치법: " + treatment);
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                });
+    }
+
+
 
     @Override
     protected void onDestroy() {
